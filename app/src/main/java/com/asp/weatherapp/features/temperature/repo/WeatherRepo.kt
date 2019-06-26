40c4cce3e1ebce18cf79ca1e26bdc5e68955ba75
Anu S Pillai <anugotta@gmail.com>
2019-06-26 17:02:28 +0530

    Code Cleanup and formatting
    Signed-off-by: Anu S Pillai <anugotta@gmail.com>

--- app/src/main/java/com/asp/weatherapp/features/temperature/repo/WeatherRepo.kt
+++ app/src/main/java/com/asp/weatherapp/features/temperature/repo/WeatherRepo.kt
@@ -2,7 +2,6 @@ package com.asp.weatherapp.features.temperature.repo
 
 import com.asp.weatherapp.api.ApiInterface
 import com.asp.weatherapp.features.temperature.data.Info
-import com.asp.weatherapp.preferences.WeatherPreferences
 import com.asp.weatherapp.utils.AppRxSchedulers
 import com.asp.weatherapp.utils.Constants.Companion.WEATHER_API_KEY
 import io.reactivex.Single
@@ -11,7 +10,6 @@ import javax.inject.Singleton
 @Singleton
 class WeatherRepo(
     var api: ApiInterface,
-    var pref: WeatherPreferences,
     var rxSchedulers: AppRxSchedulers
 ) {
 
--- app/src/main/java/com/asp/weatherapp/preferences/WeatherPreferences.kt
+++
@@ -1,9 +0,0 @@
-package com.asp.weatherapp.preferences
-
-import com.asp.weatherapp.utils.SharedPrefDelegate
-
-
-class WeatherPreferences : SharedPrefDelegate("weather_pref") {
-
-    var samplePref by stringPref("samplePref", "")
-}
